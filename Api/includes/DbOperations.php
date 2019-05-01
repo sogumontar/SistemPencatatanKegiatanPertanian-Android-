6e67bab70d2b74682a197a3635ff42e8cbaafde5
Sogumontar Hendra Simangunsong <if417010@students.del.ac.id>
2019-05-01 21:27:31 +0700

    masih API

--- Api/includes/DbOperations.php
+++ Api/includes/DbOperations.php
@@ -1,20 +1,38 @@
 <?php 
 
 	class DbOperations{
-		prinvate $con;
+		private $con;
 		function __construct(){
 			require_once dirname(__FILE__) . '/DbConnect.php';
 			$db = new DbConnect;
 			$this->con=$db->connect();
 		}
 		public function createUser($nama,$kelas,$nim){
-			$statement=$this->con->prepare("INSERT INTO mahasiswa(nama,kelas,nim),VALUES ('hend','d4Ti','asd')");
-			if($statement->execute()){
-				return USER_CREATED;
-			}else{
-				return USER_FAILURE;
-			}
+			$stmt = $this->con->prepare("INSERT INTO mahasiswa(nama,kelas,nim) VALUES(?,?,?)");
+          $stmt->bind_param("sss",$nama,$kelas,$nim);
+          if($stmt->execute()){
+            return USER_CREATED;
+          }else{
+            return USER_FAILURE;  
+          }
+          return USER_EXISTS;  
 
 		}
-		return USER_EXISTS;
+		 public function getAllUsers(){
+            $stmt = $this->con->prepare("SELECT * FROM mahasiswa;");
+            $stmt->execute(); 
+            $stmt->bind_result($id, $email, $password, $nama);
+            $users = array(); 
+            while($stmt->fetch()){ 
+                $user = array(); 
+                $user['id'] = $id; 
+                $user['nama']=$email; 
+                $user['kelas'] = $password; 
+                $user['nim'] = $nama; 
+                array_push($users, $user);
+            }             
+            return $users; 
+        }
+    
+		
 	}
--- Api/public/index.php
+++ Api/public/index.php
@@ -4,36 +4,87 @@ use Psr\Http\Message\ResponseInterface as Response;
 
 require '../vendor/autoload.php';
 require '../includes/DbConnect.php';
-$app=new \Slim\App;
-// $app = new \Slim\App;
-// $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
-//     $name = $args['name'];
-//     $response->getBody()->write("Hello, $name");
-//     $db=new DbConnect;
-//     if($db->connect()!=null){
-//     	echo "Connect Succes!";
-//     }
-//     return $response;
-// });
-$app->post('/createuser',function(Request $request,Response $response){
+require '../includes/DbOperations.php';
 
+
+$app = new \Slim\App([
+    'settings'=>[
+        'displayErrorDetails'=>true
+    ]
+]); 
+
+$app->get('/allusers', function(Request $request, Response $response){
+    $db = new DbOperations; 
+    $users = $db->getAllUsers();
+    $response_data = array();
+    $response_data['error'] = false; 
+    $response_data['users'] = $users; 
+    $response->write(json_encode($response_data));
+    return $response
+    ->withHeader('Content-type', 'application/json')
+    ->withStatus(200);  
 });
-function haveEmptyParameters($required_params, $response){
-	$error=false;
-	$error_params='';
-	$request_params=$_REQUEST;
-	foreach ($request_params as $param) {
-		if(isset($request[$param]) || strlen($request_params[$param])<=0){
-			$error=true;
-			$error_params .=$param . ', ';
-		}
-	}
+$app->post('/createuser',function(Request $request,Response $response){
+		$request_data = $request->getParsedBody();
+		$nama = $request_data['nama'];
+		$kelas = $request_data['kelas'];
+		$nim = $request_data['nim'];
 
-	if($error){
-		$error_detail=array();
-		$error_detail['error']=true;
-		$error_details['message']='Required parameters ' .substr($error_params,0,-2);
-	}
+
+	
+
+		$db = new DBOperations();
+
+		$result = $db->createUser($nama,$kelas,$nim);
+
+		if($result == USER_CREATED){
+            $message = array(); 
+            $message['error'] = false; 
+            $message['message'] = 'User created successfully';
+            $response->write(json_encode($message));
+            return $response
+                        ->withHeader('Content-type', 'application/json')
+                        ->withStatus(201);
+        }else if($result == USER_FAILURE){
+            $message = array(); 
+            $message['error'] = true; 
+            $message['message'] = 'Some error occurred';
+            $response->write(json_encode($message));
+            return $response
+                        ->withHeader('Content-type', 'application/json')
+                        ->withStatus(422);    
+        }else if($result == USER_EXISTS){
+            $message = array(); 
+            $message['error'] = true; 
+            $message['message'] = 'User Already Exists';
+            $response->write(json_encode($message));
+            return $response
+                        ->withHeader('Content-type', 'application/json')
+                        ->withStatus(422);    
+        }
+	
+	return $response
+					->withHeader('Content-type', 'application/json')
+					->withStatus(422);
+});
+function haveEmptyParameters($required_params, $request, $response){
+    $error = false; 
+    $error_params = '';
+    $request_params = $request->getParsedBody(); 
+    foreach($required_params as $param){
+        if(!isset($request_params[$param]) || strlen($request_params[$param])<=0){
+            $error = true; 
+            $error_params .= $param . ', ';
+        }
+    }
+    if($error){
+        $error_detail = array();
+        $error_detail['error'] = true; 
+        $error_detail['message'] = 'Required parameters ' . substr($error_params, 0, -2) . ' are missing or empty';
+        $response->write(json_encode($error_detail));
+    }
+    return $error; 
 }
+
 
 $app->run();
